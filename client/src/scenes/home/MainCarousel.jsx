e37aaeea768f0b40d1c18880123fb584c4e6ec34
Louistam888 <louistam888@gmail.com>
2023-02-25 23:53:18 -0500

    finished carousel

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -79,16 +79,24 @@ const MainCarousel = () => {
             padding="20px"
             borderRadius="1px"
             textAlign="left"
-            backgroundColor="absolute"
+            backgroundColor="rgb(0, 0, 0, 0.4)"
+            position="absolute"
             top="46%"
             left={isNonMobile ? "10%" : "0"}
             right={isNonMobile ? undefined : "0"}
             margin={isNonMobile ? undefined : "0 auto"}
             maxWidth={isNonMobile ? undefined : "240px"}
           >
-
+            <Typography color={shades.secondary[200]}>--NEW ITEMS</Typography>
+            <Typography variant="h1">Summer Sale</Typography>
+            <Typography 
+              fontWeight="bold" 
+              color={shades.secondary[300]} 
+              sx={{ textDecoration: "underline" }}
+            >
+              Discover More
+            </Typography>
           </Box>
-
         </Box>
       ))}
     </Carousel>
